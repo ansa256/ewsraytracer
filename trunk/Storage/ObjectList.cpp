d3127e99ceb9edfe91875ac6e2f7f2941c156c68
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2011-09-26 19:58:23 +0000

    Adding ObjectList storage method. This uses the underlying object list and iterates over all items in the list for each hit test. Useful for testing purposes.

---
+++ trunk/Storage/ObjectList.cpp
@@ -0,0 +1,43 @@
+#include "ObjectList.h"
+#include "Math/Maths.h"
+
+typedef list<GeometryObject*>::const_iterator ListIter;
+
+bool ObjectList::hit(const Ray& ray, double& tmin, ShadeRecord& sr) const {
+   bool hit = false;
+   double tcheck = HUGE_VALUE;
+   Vector3D normal;
+   Point3D localHitPoint;
+   Point3D hitPoint;
+   shared_ptr<Material> mat;
+
+   for(ListIter it = objs.begin(); it != objs.end(); ++it) {
+      if((*it)->hit(ray, tmin, sr) && (tmin < tcheck)) {
+         hit = true;
+         tcheck = tmin;
+         normal = sr.normal;
+         localHitPoint = sr.localHitPoint;
+         hitPoint = ray(tmin);
+         mat = (*it)->getMaterial();
+      }
+   }
+
+   if(hit) {
+      sr.localHitPoint = localHitPoint;
+      sr.hitPoint = hitPoint;
+      sr.normal = normal;
+      sr.material = mat;
+   }
+
+   return hit;
+}
+
+bool ObjectList::shadowHit(const Ray& ray, double& tmin) const {
+   for(ListIter it = objs.begin(); it != objs.end(); it++) {
+      if(!(*it)->ignoreShadow && (*it)->shadowHit(ray, tmin)) {
+         return true;
+      }
+   }
+
+   return false;
+}
---
+++ trunk/Storage/ObjectList.h
@@ -0,0 +1,18 @@
+#ifndef OBJECTLIST_H
+#define OBJECTLIST_H
+
+#include "Storage.h"
+
+class ObjectList : public Storage {
+
+public:
+   ObjectList() {}
+   virtual ~ObjectList() {}
+
+   virtual void setup() {}
+   virtual void setHash(Hash* hash) {}
+   virtual bool hit(const Ray& ray, double& tmin, ShadeRecord& sr) const;
+   virtual bool shadowHit(const Ray& ray, double& tmin) const;
+};
+
+#endif // OBJECTLIST_H
