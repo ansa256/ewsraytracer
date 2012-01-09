deb962bbdecf8c1afc30f98bb28e31c1e7cff093
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-01-09 04:00:18 +0000

    No longer need a .cpp file for Point2D as it is a very simple class. Added a *= operator to Point2D.

--- trunk/Math/Point2D.h
+++ trunk/Math/Point2D.h
@@ -1,28 +1,25 @@
-/*
- *  Point2D.h
- *  RayTracer
- *
- *  Created by Eric Saari on 12/12/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
-
 #ifndef _POINT2D_H_
 #define _POINT2D_H_
 
 class Point2D {
 
 public:
-   Point2D();
-   Point2D(float x, float y);
+   Point2D(float _x = 0, float _y = 0) : x(_x), y(_y) {}
    
    Point2D operator*(const float a) const;
+   Point2D& operator*=(const float a);
    
    float x, y;
 };
 
 inline Point2D Point2D::operator*(const float a) const {
    return Point2D(x * a, y * a);
+}
+
+inline Point2D& Point2D::operator*=(const float a) {
+   x *= a;
+   y *= a;
+   return *this;
 }
 
 #endif
