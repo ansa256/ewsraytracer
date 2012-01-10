1898b08ff24e08ec7d8ca84ba9a158c1cc0c2851
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-01-10 22:40:28 +0000

    No longer store ls as a class variable on PositionLight. Instead apply it to the color as soon as it is read from the configuration. This saves having to multiply color by ls every time getColor is called on position lights.

--- Lights/PositionLight.cpp
+++ Lights/PositionLight.cpp
@@ -1,12 +1,3 @@
-/*
- *  PositionLight.cpp
- *  RayTracer
- *
- *  Created by Eric Saari on 12/14/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
-
 #include "PositionLight.h"
 #include "Parser/Hash.h"
 #include "Geometry/GeometryManager.h"
@@ -14,11 +5,11 @@
 #include "Math/Maths.h"
 #include "Storage/Storage.h"
 
-PositionLight::PositionLight(float halfDistance) : Light(), color(1, 1, 1), location(), ls(1.0) {
+PositionLight::PositionLight(float halfDistance) : Light(), color(1, 1, 1), location() {
    attenuationPower = LogN(2, halfDistance);
 }
 
-PositionLight::PositionLight(const Point3D& loc, float halfDistance) : Light(), color(1, 1, 1), location(loc), ls(1.0) {
+PositionLight::PositionLight(const Point3D& loc, float halfDistance) : Light(), color(1, 1, 1), location(loc) {
    attenuationPower = LogN(2, halfDistance);
 }
 
@@ -38,7 +29,7 @@ bool PositionLight::inShadow(const Ray& ray, const ShadeRecord& sr) {
 void PositionLight::setHash(Hash* hash) {
    location.set(hash->getValue("location")->getArray());
    color.set(hash->getValue("color")->getArray());
-   ls = hash->getDouble("radiance");
+   color *= hash->getDouble("radiance");
 
    if(hash->contains("halfDistance")) {
       attenuationPower = LogN(2, hash->getDouble("halfDistance"));
@@ -47,5 +38,5 @@ void PositionLight::setHash(Hash* hash) {
 
 Color PositionLight::L(const ShadeRecord& sr) {
 //   double d = (location - sr.hitPoint).length();
-   return color * ls; // pow(d, attenuationPower);
+   return color; // pow(d, attenuationPower);
 }
