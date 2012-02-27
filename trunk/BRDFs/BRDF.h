2e6ced9cfaa0d440aedd53e2f56a837fc7b4f7a3
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-02-27 22:34:37 +0000

    Removed sampler from BRDF base class. Moved al BRDF functionality to header file.

--- trunk/BRDFs/BRDF.h
+++ trunk/BRDFs/BRDF.h
@@ -6,21 +6,15 @@
 #include "Math/Point3D.h"
 #include "Utility/ShadeRecord.h"
 
-class Sampler;
-
 class BRDF {
 
 public:
-   BRDF();
-   virtual ~BRDF();
+   BRDF() {}
+   virtual ~BRDF() {}
 
-   void setSampler(Sampler* sampler);
-   virtual Color f(const ShadeRecord& sr, const Vector3D& wo, const Vector3D& wi) const;
-   virtual Color rho(const ShadeRecord& sr, const Vector3D& wo) const;
+   virtual Color f(const ShadeRecord& sr, const Vector3D& wo, const Vector3D& wi) const { return BLACK; }
+   virtual Color rho(const ShadeRecord& sr, const Vector3D& wo) const { return BLACK; }
    virtual float getAlpha(const Point3D& p) const { return 1.0; }
-
-protected:
-   Sampler* sampler;
 };
 
 #endif
