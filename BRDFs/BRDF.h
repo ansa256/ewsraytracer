3777b1eb0e9512fe8eb135eecb37fdeab49e8812
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-02-29 05:14:55 +0000

    Fixed getAlpha function signature. It should take a ShadeRecord, not a Point3D.

--- BRDFs/BRDF.h
+++ BRDFs/BRDF.h
@@ -3,7 +3,6 @@
 
 #include "Utility/Color.h"
 #include "Math/Vector3D.h"
-#include "Math/Point3D.h"
 #include "Utility/ShadeRecord.h"
 
 class BRDF {
@@ -14,7 +13,7 @@ public:
 
    virtual Color f(const ShadeRecord& sr, const Vector3D& wo, const Vector3D& wi) const { return BLACK; }
    virtual Color rho(const ShadeRecord& sr, const Vector3D& wo) const { return BLACK; }
-   virtual float getAlpha(const Point3D& p) const { return 1.0; }
+   virtual float getAlpha(const ShadeRecord& sr) const { return 1.0; }
 };
 
 #endif
