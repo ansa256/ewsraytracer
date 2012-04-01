c2bf6323ebc0bf8a5e56bfaf173cc5ceca41d6d3
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-01 22:24:04 +0000

    Updated test scene by a) making window size variable b) using a filled ellipse.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include "Utility/SDL_Utility.h"
 #include "Shapes2D/ThickLine.h"
 #include "Shapes2D/FilledCircle.h"
+#include "Shapes2D/FilledEllipse.h"
 #include <vector>
 
 using namespace std;
+
+const int width = 800;
+const int height = 500;
 
 typedef vector<Shape2D*>::const_iterator ShapeIter;
 
@@ -36,26 +40,27 @@ int main(int argc, char **argv) {
       exit(1);
    }
 
-   SDL_Surface* screen = SDL_SetVideoMode(500, 500, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+   SDL_Surface* screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
-   SDL_Surface* surface = createSurface(500, 500);
+   SDL_Surface* surface = createSurface(width, height);
    Uint32 black = SDL_MapRGBA(surface->format, 0, 0, 0, 0);
    SDL_FillRect(surface, NULL, black);
 
    vector<Shape2D*> shapes;
 
-   Color blue(.1, .3, .8, 1);
+   Color blue(.1, .3, .8, 0);
    Color white(1, 1, 1, 1);
+   
+   int hw = width / 2;
+   int hh = height / 2;
 
-   shapes.push_back(new ThickLine(250, 250, 499, 250, 25, white, blue));
-   shapes.push_back(new ThickLine(249, 250, 0, 250, 25, blue, white));
+   shapes.push_back(new ThickLine(hw, hh, min(hw + 400, width-1) , hh, 25, white, blue));
+   shapes.push_back(new ThickLine(hw, hh, max(hw - 400, 0), hh, 25, blue, white));
 
-   blue.alpha = 0;
-   shapes.push_back(new ThickLine(250, 250, 250, 450, 25, white, blue));
-   shapes.push_back(new ThickLine(250, 249, 250, 50, 25, blue, white));
+   shapes.push_back(new ThickLine(hw, hh, hw, hh + 200, 25, white, blue));
+   shapes.push_back(new ThickLine(hw, hh, hw, hh - 200, 25, blue, white));
 
-   shapes.push_back(new FilledCircle(250, 250, 150, white, Color(.1, .3, .8, 0)));
-   shapes.push_back(new FilledCircle(250, 250, 50, white, Color(1, 1, 1, 0)));
+   shapes.push_back(new FilledEllipse(hw, hh, 125, 100, white, blue));
 
    for(ShapeIter it = shapes.begin(); it != shapes.end(); ++it) {
       (*it)->draw(surface);
