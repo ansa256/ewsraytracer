0664cd671d0a6e9b660355c197a294a6a31b1d40
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-02 04:50:26 +0000

    Adding light rays.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ const int height = 500;
 typedef vector<Shape2D*>::const_iterator ShapeIter;
 
 void run();
+int randomNoBetween(int x, int y);
+
+int randomNoBetween(int x, int y) {
+   return (rand() % (y - x + 1)) + x;
+}
 
 void run() {
    SDL_Event event;
@@ -39,6 +44,8 @@ int main(int argc, char **argv) {
       fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
       exit(1);
    }
+   
+   srand(0);
 
    SDL_Surface* screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
@@ -54,13 +61,20 @@ int main(int argc, char **argv) {
    int hw = width / 2;
    int hh = height / 2;
 
-   shapes.push_back(new ThickLine(hw, hh, min(hw + 400, width-1) , hh, 25, white, blue));
-   shapes.push_back(new ThickLine(hw, hh, max(hw - 400, 0), hh, 25, blue, white));
+   shapes.push_back(new ThickLine(hw, hh, min(hw + 400, width-1) , hh, 35, white, blue));
+   shapes.push_back(new ThickLine(hw, hh, max(hw - 400, 0), hh, 35, blue, white));
 
    shapes.push_back(new ThickLine(hw, hh, hw, hh + 200, 25, white, blue));
    shapes.push_back(new ThickLine(hw, hh, hw, hh - 200, 25, blue, white));
-
-   shapes.push_back(new FilledEllipse(hw, hh, 125, 100, white, blue));
+   
+   float angle = 100;
+   while(angle < 200) {
+      int length = randomNoBetween(100, 250);
+      shapes.push_back(new Line(hw, hh, length, angle, height, blue, white));
+      shapes.push_back(new Line(hw, hh, length, angle + 180, height, blue, white));
+      angle += randomNoBetween(1, 8);
+   }
+   shapes.push_back(new FilledEllipse(hw, hh, 100, 85, white, blue));
 
    for(ShapeIter it = shapes.begin(); it != shapes.end(); ++it) {
       (*it)->draw(surface);
