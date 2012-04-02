7abe4ded89806c6b7f8690a5e2fe8e4c1c6beb0c
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-02 04:49:00 +0000

    Added length / angle Line constructor. Fixed bugs in anti aliasing code.

--- Shapes2D/Line.h
+++ Shapes2D/Line.h
@@ -7,16 +7,17 @@
 class Line : public Shape2D {
 
 public:
-   Line(Uint16 _x1, Uint16 _y1, Uint16 _x2, Uint16 _y2, const Color& c1, const Color& c2);
+   Line(Uint32 _x1, Uint32 _y1, Uint32 _x2, Uint32 _y2, const Color& c1, const Color& c2);
+   Line(Uint32 x, Uint32 y, Uint32 length, float angle, Uint32 height, const Color& c1, const Color& c2);
 
    virtual void draw(SDL_Surface* surf);
 
 protected:
-   Uint16 x1, y1, x2, y2;
+   Uint32 x1, y1, x2, y2;
    Color color1, color2;
 
-   void horizontal(SDL_Surface* surf, Uint16 y);
-   void vertical(SDL_Surface * surf, Uint16 x);
+   void horizontal(SDL_Surface* surf, Uint32 y);
+   void vertical(SDL_Surface * surf, Uint32 x);
 };
 
 #endif
