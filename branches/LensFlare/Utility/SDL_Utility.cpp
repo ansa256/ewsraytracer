26bb1144bc2d4c97af6cef93ec635ddaecfe9ead
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-02 04:50:04 +0000

    Added setBlendColor that takes an alpha weight.

--- branches/LensFlare/Utility/SDL_Utility.cpp
+++ branches/LensFlare/Utility/SDL_Utility.cpp
@@ -64,3 +64,9 @@ void setBlendColor(SDL_Surface* surf, Uint32* pixel, const Color& c) {
 
    *pixel = SDL_MapRGBA(surf->format, r, g, b, a);
 }
+
+void setBlendColor(SDL_Surface * surf, Uint16 x, Uint16 y, const Color& c, int weight) {
+   int alpha = ((c.getAlpha() * weight) >> 8);
+   Color color(c.red, c.green, c.blue, alpha / 255.f);
+   setBlendColor(surf, x, y, color);
+}
