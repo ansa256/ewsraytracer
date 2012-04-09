d245ac4d6b56faeb4d4b977756f22fd9be068e1b
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-09 03:23:58 +0000

    Updated sun shape.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDL/SDL.h>
 #include "Utility/SDL_Utility.h"
 #include "Shapes2D/ThickLine.h"
-#include "Shapes2D/FilledCircle.h"
+#include "Shapes2D/Line.h"
 #include "Shapes2D/FilledEllipse.h"
 #include "Shapes2D/Ring.h"
 #include "Shapes2D/Fan.h"
@@ -11,20 +11,23 @@
 
 using namespace std;
 
-const int width = 960;
-const int height = 540;
-const int cx = 480;
-const int cy = 270;
+const int width = 1024;
+const int height = 768;
+const int cx = 512;
+const int cy = 384;
 
 vector<Shape2D*> shapes;
 
-Color blue(.1, .3, .8, 0);
+Color color(0.1, 0.3, 0.8, 0);
+//Color color(1, 0.7, 0.1, 0);
 Color white(1, 1, 1, 1);
 
 typedef vector<Shape2D*>::const_iterator ShapeIter;
 
 void run();
 int randomNoBetween(int x, int y);
+void light1();
+void light2();
 
 int randomNoBetween(int x, int y) {
    return (rand() % (y - x + 1)) + x;
@@ -51,37 +54,74 @@ void run() {
 }
 
 void light1() {
-   shapes.push_back(new ThickLine(cx, cy, min(cx + 400, width-1) , cy, 35, white, blue));
-   shapes.push_back(new ThickLine(cx, cy, max(cx - 400, 0), cy, 35, blue, white));
+   shapes.push_back(new ThickLine(cx, cy, min(cx + 400, width-1) , cy, 35, white, color));
+   shapes.push_back(new ThickLine(cx, cy, max(cx - 400, 0), cy, 35, color, white));
 
-   shapes.push_back(new ThickLine(cx, cy, cx, cy + 200, 25, white, blue));
-   shapes.push_back(new ThickLine(cx, cy, cx, cy - 200, 25, blue, white));
+   shapes.push_back(new ThickLine(cx, cy, cx, cy + 200, 25, white, color));
+   shapes.push_back(new ThickLine(cx, cy, cx, cy - 200, 25, color, white));
 
    float angle = 100;
    while(angle < 200) {
       int length = randomNoBetween(100, 250);
-      shapes.push_back(new Line(cx, cy, length, angle, height, blue, white));
-      shapes.push_back(new Line(cx, cy, length, angle + 180, height, blue, white));
+      shapes.push_back(new Line(cx, cy, length, angle, white, color));
+      shapes.push_back(new Line(cx, cy, length, angle + 180, white, color));
       angle += randomNoBetween(1, 8);
    }
 
-   FilledEllipse* ellipse = new FilledEllipse(cx, cy, 100, 85, white, blue);
+   FilledEllipse* ellipse = new FilledEllipse(cx, cy, 100, 85, white, color);
    ellipse->setFilter(new CosineFilter());
    shapes.push_back(ellipse);
    shapes.push_back(new Ring(cx, cy, 200, 220, Color(.3, .6, .8, .2), Color(.3, .6, .8, 0)));
 }
 
 void light2() {
-   FilledCircle* circle = new FilledCircle(cx, cy, 100, white, blue);
-   circle->setFilter(new SmoothStepFilter(0.7, 1.0));
+   FilledEllipse* circle = new FilledEllipse(cx, cy, 250, 250, white, color);
+   circle->setFilter(new SmoothStepFilter(0.05, 1.0));
    shapes.push_back(circle);
 
-   int spread = 10;
+   int spread = 15;
 
-   for(float angle = 30; angle < 180; angle += 30) {
-      shapes.push_back(new Fan(cx, cy, 300, angle, spread, height, white, blue));
-//      shapes.push_back(new Fan(cx, cy, 300, angle + 180, spread, height, blue, white));
+   Fan* fan = new Fan(cx, cy, 320, 60, spread, Color(1, 1, 1, 0.3), color);
+   fan->setFilter(new SmoothStepFilter(0.1, 1.0));
+   shapes.push_back(fan);
+
+   fan = new Fan(cx, cy, 320, 120, spread, Color(1, 1, 1, 0.3), color);
+   fan->setFilter(new SmoothStepFilter(0.1, 1.0));
+   shapes.push_back(fan);
+
+   fan = new Fan(cx, cy, 320, 240, spread, Color(1, 1, 1, 0.3), color);
+   fan->setFilter(new SmoothStepFilter(0.1, 1.0));
+   shapes.push_back(fan);
+
+   fan = new Fan(cx, cy, 320, 300, spread, Color(1, 1, 1, 0.3), color);
+   fan->setFilter(new SmoothStepFilter(0.1, 1.0));
+   shapes.push_back(fan);
+
+   float angle = 100;
+   while(angle < 210) {
+      int length = randomNoBetween(150, 250);
+      
+      fan = new Fan(cx, cy, length, angle , 2, Color(1, 1, 1, 0.3), color);
+      fan->setFilter(new SmoothStepFilter(0.5, 1.0));
+      shapes.push_back(fan);
+
+      fan = new Fan(cx, cy, length, angle + 180, 2, Color(1, 1, 1, 0.3), color);
+      fan->setFilter(new SmoothStepFilter(0.5, 1.0));
+      shapes.push_back(fan);
+      angle += randomNoBetween(5, 15);
    }
+   
+   ThickLine* thick = new ThickLine(cx, cy, cx + 600, (int)cy, 45, white, color);
+   shapes.push_back(thick);
+   shapes.push_back(new ThickLine(cx, cy, cx - 600, cy, 45, white, color));
+   
+   thick = new ThickLine(cx, cy, cx, cy + 400, 40, white, color);
+//   thick->setFilter(new SmoothStepFilter(0.1, 1.0));
+   shapes.push_back(thick);
+
+   thick = new ThickLine(cx, cy, cx, cy - 400, 40, color, white);
+//   thick->setFilter(new SmoothStepFilter(0.3, 1.0));
+   shapes.push_back(thick);
 }
 
 int main(int argc, char **argv) {
@@ -98,7 +138,7 @@ int main(int argc, char **argv) {
    Uint32 black = SDL_MapRGBA(surface->format, 0, 0, 0, 0);
    SDL_FillRect(surface, NULL, black);
 
-   light1();
+   light2();
 
    for(ShapeIter it = shapes.begin(); it != shapes.end(); ++it) {
       (*it)->draw(surface);
