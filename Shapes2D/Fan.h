40de6bb49b2e1ef74bfc641e938439a0b8aef40c
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-09 03:21:14 +0000

    Updated fan alpha values.

--- Shapes2D/Fan.h
+++ Shapes2D/Fan.h
@@ -7,12 +7,12 @@
 class Fan : public Shape2D {
 
 public:
-   Fan(int _x, int _y, int _length, float _angle, int _spread, int _height, const Color& c1, const Color& c2);
+   Fan(int _x, int _y, int _length, float _angle, int _spread, const Color& c1, const Color& c2);
 
    virtual void draw(SDL_Surface* surf);
 
 protected:
-   int x, y, length, angle, spread, height;
+   int x, y, length, angle, spread;
    Color color1, color2;
 };
 
