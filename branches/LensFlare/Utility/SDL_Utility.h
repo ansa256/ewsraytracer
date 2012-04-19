b12c83250b2743d4816e49cb1709380303b7a9ce
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-04-19 03:26:11 +0000

    Added gaussian blur effect to remove aliasing artifacts.

--- branches/LensFlare/Utility/SDL_Utility.h
+++ branches/LensFlare/Utility/SDL_Utility.h
@@ -11,5 +11,8 @@ void setBlendColor(SDL_Surface *surf, int x, int y, const Color& c);
 void setBlendColor(SDL_Surface* surf, Uint32* pixel, const Color& c);
 void setBlendColor(SDL_Surface * surf, int x, int y, const Color& c, int weight);
 
+Uint32 getPixel(SDL_Surface *surface, int x, int y);
+void setPixel(SDL_Surface* s, int x, int y, Uint32 p);
+
 #endif
 
