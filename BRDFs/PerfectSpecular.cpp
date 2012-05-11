2ec0addc172906699340eeb800f12cbfd7ddd485
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-05-11 02:09:18 +0000

    Removed kr. Hopefully fixed weird bug.

--- BRDFs/PerfectSpecular.cpp
+++ BRDFs/PerfectSpecular.cpp
@@ -1,23 +1,9 @@
 #include "PerfectSpecular.h"
 #include "Fresnel.h"
 
-PerfectSpecular::PerfectSpecular() : BRDF(BxDFType(REFLECT | SPECULAR)), kr(0.75), color(1.0, 1.0, 1.0) {
-   fresnel = new FresnelDielectric(0, kr);
+PerfectSpecular::PerfectSpecular() : BRDF(BxDFType(REFLECT | SPECULAR)), color(1.0, 1.0, 1.0) {
+   fresnel = new FresnelConductor(1.0, 1.5);
 }
-
-void PerfectSpecular::setKr(const float k) { 
-   kr = k; 
-   delete fresnel;
-   fresnel = new FresnelConductor(309.95, 1.84);
-}
-
-//Color PerfectSpecular::sample_f(const ShadeRecord& sr, const Vector3D& wo, Vector3D& wi) const {
-//   float ndotwo = sr.normal.dot(wo);
-//
-//   // Set wi = the reflection of wo throught the surface normal
-//   wi = -wo + sr.normal * 2.0 * ndotwo;
-//   return color * kr / fabs(sr.normal.dot(wi));
-//}
 
 Color PerfectSpecular::sample_f(const ShadeRecord& sr, const Vector3D& wo, Vector3D& wi, float& pdf) const {
    pdf = 1.f;
