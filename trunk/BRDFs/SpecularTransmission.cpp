d6b1b684c48773780bdaa6f76c330779ecaafede
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-05-25 21:46:51 +0000

    Updated sphere test scene. Updated transmission BRDF.

--- trunk/BRDFs/SpecularTransmission.cpp
+++ trunk/BRDFs/SpecularTransmission.cpp
@@ -11,39 +11,19 @@ SpecularTransmission::SpecularTransmission(const Color& c, float ei, float et) :
 
 Color SpecularTransmission::sample_f(const ShadeRecord& sr, const Vector3D& wo, Vector3D& wi, float& pdf) const {
    float ndotwo = sr.normal.dot(wo);
-   bool entering = ndotwo > 0.;
-//   float ei = etai, et = etat;
-   
-//   if (!entering) {
-//      swap(ei, et);
-//   }
-//   float eta = ei / et;
+   Vector3D n = sr.normal;
 
-   // Compute transmitted ray direction
-/*   
-   float sini2 = max(0.f, 1.f - ndotwo*ndotwo);
-   float eta = ei / et;
-   float sint2 = eta * eta * sini2;
-
-   // Handle total internal reflection for transmission
-   if (sint2 >= 1.) {
-      return ZERO;
-   }
-
-   float cost = sqrtf(max(0.f, 1.f - sint2));
-   if (entering) cost = -cost;
-   float sintOverSini = eta;
-   wi = Vector3D(sintOverSini * -wo.x, sintOverSini * -wo.y, cost);
-*/
-   
-   float eta = 1.6f;
-   if(!entering) {
+   float eta = 1.5f;
+   if(ndotwo < 0) {
       eta = 1.f / eta;
+      n = -n;
+      ndotwo = -ndotwo;
    }
+
    float temp = 1.f - (1.f - ndotwo * ndotwo) / (eta * eta);
    float cosTheta2 = sqrtf(temp);
-   wi = -wo / eta - sr.normal * (cosTheta2 - ndotwo / eta);
-   
+   wi = -wo / eta - n * (cosTheta2 - ndotwo / eta);
+
    pdf = 1.f;
 //   float F = fresnel.evaluate(ndotwo);
 //   return color * (1.f - F) / fabs(sr.normal.dot(wi));
