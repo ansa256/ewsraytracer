462055c7106d87af1aa1822b763a013e090b3928
esaari1@gmail.com <esaari1@gmail.com@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2012-06-04 01:54:18 +0000

    Correctly set ray depth.

--- trunk/Math/Ray.cpp
+++ trunk/Math/Ray.cpp
@@ -4,7 +4,7 @@
 Ray::Ray() : origin(Point3D(0, 0, 0)), direction(Vector3D(0, 0, -1)), depth(0), tHit(HUGE_VALUE) {
 }
 
-Ray::Ray(const Point3D& o, const Vector3D& d, int depth) : origin(o), direction(d), depth(0), tHit(HUGE_VALUE) {
+Ray::Ray(const Point3D& o, const Vector3D& d, int _depth) : origin(o), direction(d), depth(_depth), tHit(HUGE_VALUE) {
 }
 
 Ray::Ray(const Ray& ray) : origin(ray.origin), direction(ray.direction), depth(ray.depth), tHit(HUGE_VALUE) {
